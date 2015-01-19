3afa43a6cc851e3bdc81a0c094f572bbeb867721
ckonethingy <calvinflinders3@hotmail.com>
2015-01-19 14:55:21 +1100

    more basic stuff

--- pythonChess.py
+++ pythonChess.py
@@ -1,1 +1,23 @@
-print "Hello World"	
+import sys
+
+
+chessboard = (
+    '         \n'  #   0 -  9
+    '         \n'  #  10 - 19
+    ' rnbqkbnr\n'  #  20 - 29
+    ' pppppppp\n'  #  30 - 39
+    ' ........\n'  #  40 - 49
+    ' ........\n'  #  50 - 59
+    ' ........\n'  #  60 - 69
+    ' ........\n'  #  70 - 79
+    ' PPPPPPPP\n'  #  80 - 89
+    ' RNBQKBNR\n'  #  90 - 99
+    '         \n'  # 100 -109
+    '          '   # 110 -119
+)
+#thanks thomasahle/sunfish/blob/master/sunfish.py
+
+def printGameState():
+	print chessboard
+
+printGameState()
